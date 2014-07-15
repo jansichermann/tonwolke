a1910383ede86a731a42343a6d053b42c5fcf845
Jan Sichermann <jansichermann@gmail.com>
2014-07-14 23:36:02 -0400

    header comments

--- tonwolke/src/KeychainManager+TW.h
+++ tonwolke/src/KeychainManager+TW.h
@@ -4,6 +4,8 @@
 
 /**
  Created by jan on 7/8/14. Copyright (c) 2014 Jan Sichermann. All rights reserved.
+ 
+ Set and retrieve the token from the keychain
  */
 
 
--- tonwolke/src/KeychainManager.h
+++ tonwolke/src/KeychainManager.h
@@ -4,6 +4,8 @@
 
 /**
  Created by jan on 5/3/14. Copyright (c) 2014 Jan Sichermann. All rights reserved.
+ 
+ An interface to dealing with the keychain
  */
 
 
